3e8e27c5235174eb9c6e3aa859298bcdc4ab0047
garaemon <garaemon@gmail.com>
2012-12-14 14:47:27 +0900

    implementing menu

---
+++ less-server/public/javascripts/pre.js
@@ -0,0 +1,9 @@
+
+$(function() {
+    // writing meta section immediately for smartphone devices
+    $("head").append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">');
+
+    // adding hidden-phone class...
+    $("table#makebanner").addClass("hidden-phone");
+    
+});
