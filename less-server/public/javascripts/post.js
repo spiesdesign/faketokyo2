dcf9a5d86ac6a1ede7396fb0c437fa6f456481b6
garaemon <garaemon@gmail.com>
2012-12-14 17:35:24 +0900

    updating

--- less-server/public/javascripts/post.js
+++ less-server/public/javascripts/post.js
@@ -1,4 +1,32 @@
 $(function() {
-    
+    $(function() {
+        var firstp = true;
+        $("#thumbFeatures div a").each(function() {
+            var $a = $(this);
+            var $img = $a.find("img");
+            //console.log($img.attr("src"));
+            // adding image
+            $("#sale-slideshow ul")
+                .append('<li>'
+                        + '<a href="'
+                        + $a.attr("href")
+                        + '">'
+                        + '<img src="'
+                        + $img.attr("src")
+                        + '" />'
+                        + '</a>'
+                        + '</li>');
+            // adding link
+            if (firstp) {
+                firstp = false;
+                $("#sale-slideshow .next-link")
+                    .before('<a href="#" class="active">&bull;</a>');
+            }
+            else {
+                $("#sale-slideshow .next-link")
+                    .before('<a href="#">&bull;</a>');
+            }
+        });
+    });
 });
 
